a56ba7c0d83ee56f166690e9a33590bc01f05ace
Clarice Bouwer <clarice.bouwer@dstvdm.com>
2018-04-17 19:24:34 +0200

    Add decompressor feature and tests

---
+++ Codec/Codec.Core/Decompressor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Codec.Core
+{
+    public class Decompressor
+    {
+        public string Decompress(string input)
+        {
+            if (input == null) throw new ArgumentNullException(nameof(input), "Argument cannot be null.");
+            if (string.IsNullOrWhiteSpace(input)) return string.Empty;
+
+            var inflated = Inflate(input);
+            return inflated;
+        }
+
+        private static string Inflate(string input)
+        {
+            var output = new StringBuilder();
+            for (var i = 0; i < input.Length; i++)
+            {
+                var text = Iterate(input, i);
+                output.Append(text);
+            }
+
+            return output.ToString();
+        }
+
+        private static string Iterate(string input, int i)
+        {
+            var output = new StringBuilder();
+            if ((i + 1) % 2 == 0)
+            {
+                try
+                {
+                    var count = int.Parse(input[i].ToString());
+                    var characters = new string(Enumerable.Repeat(input[i - 1], count).ToArray());
+                    output.Append(characters);
+                }
+                catch (FormatException)
+                {
+                    throw new InvalidOperationException($"Expected a number but got a \"{input[i - 1]}\".");
+                }
+            }
+            return output.ToString();
+        }
+    }
+}
---
+++ Codec/Codec.Tests/DecompressorShould.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Codec.Core;
+using NUnit.Framework;
+
+namespace Codec.Tests
+{
+    [TestFixture]
+    public class DecompressorShould
+    {
+        [Test]
+        public void Disallow_missing_input()
+        {
+            // Arrange
+            var decompressor = new Decompressor();
+
+            // Act
+            string Actual() => decompressor.Decompress(null);
+
+            // Assert
+            Assert.That(Actual, Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void Returns_an_empty_string()
+        {
+            // Arrange
+            var decompressor = new Decompressor();
+
+            // Act
+            var actual = decompressor.Decompress(string.Empty);
+
+            // Assert
+            Assert.That(actual, Is.Empty);
+        }
+
+        [Test]
+        public void Decompress_string()
+        {
+            // Arrange
+            const string input = "a2b3c5a2";
+            var decompressor = new Decompressor();
+
+            // Act
+            var actual = decompressor.Decompress(input);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo("aabbbcccccaa"));
+        }
+
+        [Test]
+        public void Break_decompression_when_string_is_malformed_string()
+        {
+            // Arrange
+            const string input = "aabbbcccccaa";
+            var decompressor = new Decompressor();
+
+            // Act
+            string Actual() => decompressor.Decompress(input);
+
+            // Assert
+            Assert.That(Actual, Throws.TypeOf<InvalidOperationException>());
+        }
+    }
+}
