67a3c14a0dc7b7266dfa31231091305e662bb58d
Clarice Bouwer <clarice.bouwer@dstvdm.com>
2018-04-17 06:43:47 +0200

    Add Codec solution with compressor feature and tests

---
+++ Codec/Codec.Core/Compressor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace Codec.Core
+{
+    public class Compressor
+    {
+        public string Compress(string input)
+        {
+            if (input == null) throw new ArgumentNullException(nameof(input), "Argument cannot be null.");
+            if (string.IsNullOrWhiteSpace(input)) return string.Empty;
+            if (input.Length == 1) return $"{input}1";
+
+            var deflated = Deflate(input);
+            return deflated;
+        }
+
+        private static string Deflate(string input)
+        {
+            var total = 0;
+            var output = new StringBuilder();
+            for (var index = 0; index < input.Length; index++)
+            {
+                var iteration = Iterate(input, index, total);
+                total = iteration.total;
+                output.Append(iteration.output);
+            }
+
+            return output.ToString();
+        }
+
+        private static (string output, int total) Iterate(string input, int index, int total)
+        {
+            var current = input[index].ToString();
+            var next = NextIndex(input, index);
+            var output = string.Empty;
+            total += 1;
+            if (next != current)
+            {
+                output = $"{current}{total}";
+                total = 0;
+            }
+            return (output, total);
+        }
+
+        private static string NextIndex(string input, int index)
+        {
+            return index + 1 < input.Length ? input[index + 1].ToString() : string.Empty;
+        }
+    }
+}
---
+++ Codec/Codec.Tests/CompressorShould.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Codec.Core;
+using NUnit.Framework;
+
+namespace Codec.Tests
+{
+    [TestFixture]
+    public class CompressorShould
+    {
+        [Test]
+        public void Disallow_missing_input()
+        {
+            // Arrange
+            var compressor = new Compressor();
+
+            // Act
+            string Actual() => compressor.Compress(null);
+
+            // Assert
+            Assert.That(Actual, Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void Returns_an_empty_string()
+        {
+            // Arrange
+            var compressor = new Compressor();
+
+            // Act
+            var actual = compressor.Compress(string.Empty);
+
+            // Assert
+            Assert.That(actual, Is.Empty);
+        }
+
+        [Test]
+        public void Return_letter_and_one_instance_when_one_letter_is_provided()
+        {
+            // Arrange
+            const string input = "a";
+            var compressor = new Compressor();
+
+            // Act
+            var actual = compressor.Compress(input);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo("a1"));
+        }
+
+        [Test]
+        public void Compress_string()
+        {
+            // Arrange
+            const string input = "aabbbcccccaa";
+            var compressor = new Compressor();
+
+            // Act
+            var actual = compressor.Compress(input);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo("a2b3c5a2"));
+        }
+    }
+}
